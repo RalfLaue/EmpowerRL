afa4b85f307afc88811e7b7342c761129006a6a1
Ralf Laue <ralf.laue@vodafone.com>
2023-12-05 14:23:55 +0100

    test

--- HelloWorld_1/HelloWorld_1.cs
+++ HelloWorld_1/HelloWorld_1.cs
@@ -17,6 +17,7 @@ namespace HelloWorld_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
+			//test
 			engine.GenerateInformation("Hello World");
 		}
 	}
